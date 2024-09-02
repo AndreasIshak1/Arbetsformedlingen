0664ed7eee49bea9c9cb9be973c56d54ea6ef90d
leonarddeutgen <leonard.deutgen@medieinstitutet.se>
2024-09-02 14:34:59 +0200

    :lipstick: Corrected style singlePage

--- src/components/SingleHtml.tsx
+++ src/components/SingleHtml.tsx
@@ -1,39 +1,52 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
 import { IHits } from "../models/IHits";
 import axios from "axios";
-import { InfoCardHeadingLevel, InfoCardType, InfoCardVariation, LayoutBlockVariation, LinkVariation } from "@digi/arbetsformedlingen";
-import { DigiInfoCard, DigiLayoutBlock, DigiLayoutContainer, DigiLinkExternal } from "@digi/arbetsformedlingen-react";
-import parse from 'html-react-parser';
+import {
+  InfoCardHeadingLevel,
+  InfoCardType,
+  InfoCardVariation,
+  LayoutBlockVariation,
+  LinkVariation,
+} from "@digi/arbetsformedlingen";
+import {
+  DigiInfoCard,
+  DigiLayoutBlock,
+  DigiLayoutContainer,
+  DigiLinkExternal,
+} from "@digi/arbetsformedlingen-react";
+import parse from "html-react-parser";
 export const SingleHtml = () => {
   const { id } = useParams<{ id: string }>();
   const [singleAd, setSingAd] = useState<IHits>();
 
   useEffect(() => {
     const getSingelApi = async () => {
-      const response = await axios.get<IHits>(`https://jobsearch.api.jobtechdev.se/ad/${id}`);
+      const response = await axios.get<IHits>(
+        `https://jobsearch.api.jobtechdev.se/ad/${id}`
+      );
       if (response) {
         setSingAd(response.data);
       }
-    }
+    };
     getSingelApi();
-  }, [id])
-
+  }, [id]);
 
   return (
     <>
-
-
-
       <DigiLayoutContainer>
-
         <DigiLayoutBlock afVariation={LayoutBlockVariation.PRIMARY}>
           <section className="infoLeft">
-            <img src={singleAd?.logo_url} alt="logo" />
+            <div>
+              <img src={singleAd?.logo_url} alt="logo" />
+            </div>
             <h2>{singleAd?.headline}</h2>
             <p>Tjänst typ: {singleAd?.working_hours_type.label}</p>
             <p>{singleAd?.employer.name}</p>
-            <p>{singleAd?.workplace_address.postcode} {singleAd?.workplace_address.city}</p>
+            <p>
+              {singleAd?.workplace_address.postcode}{" "}
+              {singleAd?.workplace_address.city}
+            </p>
 
             <DigiInfoCard
               afHeading="Jag är ett infokort"
@@ -42,38 +55,45 @@ export const SingleHtml = () => {
               afLinkHref="Frivillig länk"
               afLinkText="Frivillig länktext"
               afVariation={InfoCardVariation.SECONDARY}
-            //afSize={infoCardSize.STANDARD}
+              //afSize={infoCardSize.STANDARD}
             >
-              <p>{parse(singleAd?.description.text_formatted ? singleAd?.description.text_formatted : "")}</p>
+              <p>
+                {parse(
+                  singleAd?.description.text_formatted
+                    ? singleAd?.description.text_formatted
+                    : ""
+                )}
+              </p>
             </DigiInfoCard>
           </section>
-
-        </DigiLayoutBlock >
-
+        </DigiLayoutBlock>
 
         <DigiInfoCard
           afHeading="Kontakt information"
           afHeadingLevel={InfoCardHeadingLevel.H2}
           afType={InfoCardType.TIP}
           afVariation={InfoCardVariation.PRIMARY}
-        //afSize={infoCardSize.STANDARD}
+          //afSize={infoCardSize.STANDARD}
         >
           <DigiLinkExternal
-            afHref={singleAd?.application_details.url ? singleAd?.application_details.url : "skapa komponent"}
+            afHref={
+              singleAd?.application_details.url
+                ? singleAd?.application_details.url
+                : "skapa komponent"
+            }
             afTarget="_blank"
             afVariation={LinkVariation.LARGE}
           >
             Besök hemsida
           </DigiLinkExternal>
-          <p>{singleAd?.employer.name}
-          </p>
+          <p>{singleAd?.employer.name}</p>
           <p>{singleAd?.workplace_address.region}</p>
-          <p>Adress :{singleAd?.workplace_address.street_address}, {singleAd?.workplace_address.postcode}</p>
+          <p>
+            Adress :{singleAd?.workplace_address.street_address},{" "}
+            {singleAd?.workplace_address.postcode}
+          </p>
         </DigiInfoCard>
-
-
       </DigiLayoutContainer>
     </>
-  )
-}
-
+  );
+};
