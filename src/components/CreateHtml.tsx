709c5da02f4c9f4dc904e7682c23e2f663d8efba
leonarddeutgen <leonard.deutgen@medieinstitutet.se>
2024-09-02 15:02:23 +0200

    :boom:
    Co-authored-by: Andreas <AndreasIshak1@users.noreply.github.com>
    Co-authored-by: Mona <MonaAndre@users.noreply.github.com>

--- src/components/CreateHtml.tsx
+++ src/components/CreateHtml.tsx
@@ -1,27 +1,41 @@
-import { DigiLayoutContainer } from "@digi/arbetsformedlingen-react"
-import { useContext, } from "react"
+import {
+  DigiLayoutBlock,
+  DigiLayoutContainer,
+} from "@digi/arbetsformedlingen-react";
+import { useContext } from "react";
 import { AdContext } from "../context/AdContext";
 import { useNavigate } from "react-router-dom";
+import { LayoutBlockVariation } from "@digi/arbetsformedlingen";
 
 //ta emot input här
 export const CreateHtml = () => {
-
-  const { hits } = useContext(AdContext)
+  const { hits } = useContext(AdContext);
   const navigate = useNavigate();
-
 
   return (
     <>
-      {hits.map((ads) => {
-        return (
-          <DigiLayoutContainer onClick={() => navigate(`/ad/${ads.id}`)} afVerticalPadding key={ads.id} >
-            <h2>{ads.headline}</h2>
-            <p>Tjänst typ: {ads.working_hours_type.label}</p>
-            <p>{ads.employer.name}</p>
-            <p>{ads.workplace_address.postcode} {ads.workplace_address.city}</p>
-          </DigiLayoutContainer >)
-
-      })}
+      <DigiLayoutContainer>
+        {hits.map((ads) => {
+          return (
+            <DigiLayoutBlock
+              afVariation={LayoutBlockVariation.PRIMARY}
+              className="homePage"
+              onClick={() => navigate(`/ad/${ads.id}`)}
+              afVerticalPadding
+              key={ads.id}
+            >
+              <section className="infoLeft">
+                <h2>{ads.headline}</h2>
+                <p>Tjänst typ: {ads.working_hours_type.label}</p>
+                <p>{ads.employer.name}</p>
+                <p>
+                  {ads.workplace_address.postcode} {ads.workplace_address.city}
+                </p>
+              </section>
+            </DigiLayoutBlock>
+          );
+        })}
+      </DigiLayoutContainer>
     </>
-  )
-}
+  );
+};
