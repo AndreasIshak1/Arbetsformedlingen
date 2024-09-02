33858becd3e75bdd94daf160cae8e67edf0e9ea1
Andreas Ishak <Andreas.Ishak@medieinstitutet.se>
2024-09-02 13:13:33 +0200

    added styling for single page :boom:

--- src/components/SingleHtml.tsx
+++ src/components/SingleHtml.tsx
@@ -2,8 +2,8 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
 import { IHits } from "../models/IHits";
 import axios from "axios";
-import { LayoutBlockVariation, LinkVariation } from "@digi/arbetsformedlingen";
-import { DigiLayoutBlock, DigiLayoutContainer, DigiLinkExternal } from "@digi/arbetsformedlingen-react";
+import { InfoCardHeadingLevel, InfoCardType, InfoCardVariation, LayoutBlockVariation, LinkVariation } from "@digi/arbetsformedlingen";
+import { DigiInfoCard, DigiLayoutBlock, DigiLayoutContainer, DigiLinkExternal } from "@digi/arbetsformedlingen-react";
 import parse from 'html-react-parser';
 export const SingleHtml = () => {
   const { id } = useParams<{ id: string }>();
@@ -21,31 +21,52 @@ export const SingleHtml = () => {
 
 
   return (
-
     <>
-      <DigiLayoutBlock afVariation={LayoutBlockVariation.PRIMARY}>
-
-        <DigiLayoutContainer afVerticalPadding>
-          <img src={singleAd?.logo_url} alt="logo" />
-          <h2>{singleAd?.headline}</h2>
-          <p>Tjänst typ: {singleAd?.working_hours_type.label}</p>
-          <p>{singleAd?.employer.name}</p>
-          <p>{singleAd?.workplace_address.postcode} {singleAd?.workplace_address.city}</p>
 
 
+
+      <DigiLayoutContainer afVerticalPadding>
+
+        <DigiLayoutBlock afVariation={LayoutBlockVariation.PRIMARY}>
+
+          <section className="infoLeft">
+            <img src={singleAd?.logo_url} alt="logo" />
+            <h2>{singleAd?.headline}</h2>
+            <p>Tjänst typ: {singleAd?.working_hours_type.label}</p>
+            <p>{singleAd?.employer.name}</p>
+            <p>{singleAd?.workplace_address.postcode} {singleAd?.workplace_address.city}</p>
+
+            <p>{parse(singleAd?.description.text_formatted ? singleAd?.description.text_formatted : "")}</p>
+
+          </section>
+        </DigiLayoutBlock >
+
+
+
+
+        <DigiInfoCard
+          afHeading="Kontakt information"
+          afHeadingLevel={InfoCardHeadingLevel.H2}
+          afType={InfoCardType.TIP}
+          afVariation={InfoCardVariation.PRIMARY}
+        //afSize={infoCardSize.STANDARD}
+        >
           <DigiLinkExternal
             afHref={singleAd?.application_details.url ? singleAd?.application_details.url : "skapa komponent"}
             afTarget="_blank"
             afVariation={LinkVariation.LARGE}
           >
-            Jag är en extern länk
+            Besök hemsida
           </DigiLinkExternal>
+          <p>
+            Det här är bara ord för att illustrera hur det ser ut med text inuti. Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit. Suspendisse commodo egestas elit in consequat. Proin in ex consectetur,
+            laoreet augue sit amet, malesuada tellus.
+          </p>
+        </DigiInfoCard>
 
-          <p>{parse(singleAd?.description.text_formatted ? singleAd?.description.text_formatted : "")}</p>
 
-        </DigiLayoutContainer>
-
-      </DigiLayoutBlock>
+      </DigiLayoutContainer>
     </>
   )
 }
--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -3,9 +3,9 @@ import { Outlet } from "react-router-dom"
 export const Layout = () => {
   return (
     <>
-      <main>
+      
         <Outlet />
-      </main>
+    
     </>
   )
 }
