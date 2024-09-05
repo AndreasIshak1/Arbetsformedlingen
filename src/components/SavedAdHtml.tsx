35b238ab311ca8139fa88eb4e3012c728a276d63
Andreas Ishak <Andreas.Ishak@medieinstitutet.se>
2024-09-05 09:38:40 +0200

    added button for navigating from saved ad :fire:

--- src/components/SavedAdHtml.tsx
+++ src/components/SavedAdHtml.tsx
@@ -4,17 +4,20 @@ import { DigiLayoutBlock, DigiTypography } from "@digi/arbetsformedlingen-react"
 import { LayoutBlockVariation } from "@digi/arbetsformedlingen"
 import { ActionType } from "../reducers/buttonReducer"
 import { Button } from "./Button"
+import { useNavigate } from "react-router-dom"
 
 export const SavedAdHtml = () => {
   const { savedAds } = useContext(AdContext)
+  const navigate = useNavigate()
 
+  // onClick={() => navigate(`/ad/${ads.id}
 
   return <>
     {
       savedAds.map((ad) => {
         return (
-          <DigiLayoutBlock afVariation={LayoutBlockVariation.SYMBOL} className="savedAdBlock">
-            <DigiTypography>
+          <DigiLayoutBlock afVariation={LayoutBlockVariation.SYMBOL} className="savedAdBlock" >
+            <DigiTypography onClick={() => navigate(`/ad/${ad.adValue.id}`)}>
               <h2>{ad.adValue.headline}</h2>
               <p>{ad.adValue.employer.name}</p>
               <p> Typ av tjänst: {ad.adValue.working_hours_type.label}</p>
